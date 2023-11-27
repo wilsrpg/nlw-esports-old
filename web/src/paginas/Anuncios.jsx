2b5de8c3465e8e967d4ff4f93761ae3f66c30be9
wilsrpg <wilson.rpg@hotmail.com>
2023-11-27 16:15:31 -0300

    só adiciona propriedade ao filtro se houver valor (não adiciona null)

--- web/src/paginas/Anuncios.jsx
+++ web/src/paginas/Anuncios.jsx
@@ -16,36 +16,55 @@ export default function Anuncios() {
 
   useEffect(()=>{
     const dados = {};
-    dados.jogo = urlParams.get('jogo');
-    dados.nome = urlParams.get('nome');
-    dados.opcoesNome = urlParams.get('opcoesNome');
-    dados.opcoesTempo = urlParams.get('opcoesTempo');
-    dados.tempoDeJogoAnos = urlParams.get('tempoDeJogoAnos');
-    dados.tempoDeJogoMeses = urlParams.get('tempoDeJogoMeses');
+    if (urlParams.get('jogo'))
+      dados.jogo = urlParams.get('jogo');
+    if (urlParams.get('nome'))
+      dados.nome = urlParams.get('nome');
+    if (urlParams.get('opcoesNome'))
+      dados.opcoesNome = urlParams.get('opcoesNome');
+    if (urlParams.get('opcoesTempo'))
+      dados.opcoesTempo = urlParams.get('opcoesTempo');
+    if (urlParams.get('tempoDeJogoAnos'))
+      dados.tempoDeJogoAnos = urlParams.get('tempoDeJogoAnos');
+    if (urlParams.get('tempoDeJogoMeses'))
+      dados.tempoDeJogoMeses = urlParams.get('tempoDeJogoMeses');
     if (dados.opcoesTempo == 'entre') {
-      dados.tempoDeJogoAnos2 = urlParams.get('tempoDeJogoAnos2');
-      dados.tempoDeJogoMeses2 = urlParams.get('tempoDeJogoMeses2');
+      if (urlParams.get('tempoDeJogoAnos2'))
+        dados.tempoDeJogoAnos2 = urlParams.get('tempoDeJogoAnos2');
+      if (urlParams.get('tempoDeJogoMeses2'))
+        dados.tempoDeJogoMeses2 = urlParams.get('tempoDeJogoMeses2');
     }
-    dados.quando = urlParams.get('quando');
-    dados.de = urlParams.get('de');
-    dados.ate = urlParams.get('ate');
+    if (urlParams.get('quando'))
+      dados.quando = urlParams.get('quando');
+    if (urlParams.get('de'))
+      dados.de = urlParams.get('de');
+    if (urlParams.get('ate'))
+      dados.ate = urlParams.get('ate');
     let l = 2;
     while (urlParams.get('quando'+l) || urlParams.get('de'+l) || urlParams.get('ate'+l)) {
-      dados['quando'+l] = urlParams.get('quando'+l);
-      dados['de'+l] = urlParams.get('de'+l);
-      dados['ate'+l] = urlParams.get('ate'+l);
+      if (urlParams.get('quando'+l))
+        dados['quando'+l] = urlParams.get('quando'+l);
+      if (urlParams.get('de'+l))
+        dados['de'+l] = urlParams.get('de'+l);
+      if (urlParams.get('ate'+l))
+        dados['ate'+l] = urlParams.get('ate'+l);
       l++;
     }
     l--;
-    if (l > 1)
+    if (l > 1 && urlParams.get('opcoesDisponibilidade'))
       dados.opcoesDisponibilidade = urlParams.get('opcoesDisponibilidade');
     dados.qtdeFiltrosDisponibilidade = 0;
     if (dados.quando || dados.de || dados.ate)
       dados.qtdeFiltrosDisponibilidade = l;
-    dados.usaChatDeVoz = urlParams.get('usaChatDeVoz');
-    dados.resultadosPorPagina = urlParams.get('resultadosPorPagina');
-    dados.emOrdem = urlParams.get('emOrdem');
-    dados.ordenarPor = urlParams.get('ordenarPor');
+    if (urlParams.get('usaChatDeVoz'))
+      dados.usaChatDeVoz = urlParams.get('usaChatDeVoz');
+    if (urlParams.get('resultadosPorPagina'))
+      dados.resultadosPorPagina = urlParams.get('resultadosPorPagina');
+    if (urlParams.get('emOrdem'))
+      dados.emOrdem = urlParams.get('emOrdem');
+    if (urlParams.get('ordenarPor'))
+      dados.ordenarPor = urlParams.get('ordenarPor');
+
     //console.log(dados);
     if (componenteExiste)
       definirFiltros(dados);
