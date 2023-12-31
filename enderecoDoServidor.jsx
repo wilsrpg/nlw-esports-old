2f2c2d0f4b0dad85610352969454ff01bca6d962
wilsrpg <wilson.rpg@hotmail.com>
2023-12-31 01:57:24 -0300

    endereço padrão=localhost

--- enderecoDoServidor.jsx
+++ enderecoDoServidor.jsx
@@ -1,2 +1,3 @@
-export const SERVIDOR = 'http://192.168.0.189:3333';
 export const PORTA = '3333';
+// export const SERVIDOR = 'http://192.168.0.189:'+PORTA;
+export const SERVIDOR = 'http://localhost:'+PORTA;
